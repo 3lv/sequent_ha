52de2937e52d875d6c96f66af47ec193ac48074a
3lv <cristian.vlad.vladutu@gmail.com>
2024-04-06 17:50:29 +0300

    Changed structure

--- custom_components/SMioplus_relay/switch.py
+++ custom_components/SMioplus_relay/switch.py
@@ -4,20 +4,18 @@ SMioplus = libioplus
 import logging
 
 from homeassistant.const import (
-	CONF_NAME, CONF_PORT, CONF_VALUE_TEMPLATE
+	CONF_NAME
 )
 
 from homeassistant.components.light import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import SwitchEntity
 
-CONF_RELAY="relay"
-CONF_STACK="stack"
+CONF_STACK = "stack"
+CONF_CHAN = "chan"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-	vol.Required(CONF_RELAY): cv.string,
-#    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
-	vol.Optional(CONF_PORT, default='0x3F'): cv.string,
+	vol.Required(CONF_CHAN): cv.string,
 	vol.Optional(CONF_NAME, default='relay'): cv.string,
 	vol.Optional(CONF_STACK, default=0): cv.string,
 })
@@ -25,22 +23,19 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-	"""Setup the 8relay platform."""
+	"""Setup the Home Automation relay switches"""
 	add_devices([Relay(
-		port=config.get(CONF_PORT),
-#        value_template = config.get(CONF_VALUE_TEMPLATE),
 		name=config.get(CONF_NAME),
         stack=config.get(CONF_STACK),
-        relay=config.get(CONF_RELAY)
+        chan=config.get(CONF_CHAN)
 	)])
 
 class Relay(SwitchEntity):
-    """Sequent Microsystems Home Automation"""
-    def __init__(self, port, name, stack, relay):
-        self._port = int(port, 16)
+    """Sequent Microsystems Home Automation Relay Switch"""
+    def __init__(self, name, stack, chan):
         self._name = name
-        self._relay = int(relay)
         self._stack = int(stack)
+        self._chan = int(chan)
     @property
     def name(self):
         """Return the name of the switch"""
@@ -50,7 +45,7 @@ class Relay(SwitchEntity):
     def is_on(self):
         """Return true if relay is on."""
         try:
-            if SMioplus.getRelayCh(self._stack, self._relay) == 0:
+            if SMioplus.getRelayCh(self._stack, self._chan) == 0:
                 return False
             else:
                 return True
@@ -61,13 +56,13 @@ class Relay(SwitchEntity):
     def turn_on(self, **kwargs):
         """Turn the relay on."""
         try:
-            SMioplus.setRelayCh(self._stack, self._relay, 1)
+            SMioplus.setRelayCh(self._stack, self._chan, 1)
         except Exception as ex:
             _LOGGER.error("Relay turn ON failed, %e", ex)
 
     def turn_off(self, **kwargs):
         """Turn the relay off."""
         try:
-            SMioplus.setRelayCh(self._stack, self._relay, 0)
+            SMioplus.setRelayCh(self._stack, self._chan, 0)
         except Exception as ex:
             _LOGGER.error("Relay turn OFF failed, %e", ex);
