696f225ca505065885aec9b2c93a53b0c3bd3a39
3lv <cristian.vlad.vladutu@gmail.com>
2024-04-09 21:35:31 +0300

    debug:

--- custom_components/SMmultiio/number.py
+++ custom_components/SMmultiio/number.py
@@ -119,16 +119,18 @@ class Number(NumberEntity):
         com = SM_NUMBER_MAP[self._type]["com"]
         _SM_get = getattr(self._SM, com["get"])
         self._SM_get = _SM_get
-        argno = len(signature(_SM_get).parameters)
-        if argno == 0:
+        #argno = len(signature(_SM_get).parameters)
+        #if argno == 0:
+        if self._type == "motor":
             # It doesn't use stack level, add void parameter
             def _aux_SM_get(self, _):
                 _SM_get(self)
             self._SM_get = _aux_SM_get
         _SM_set = getattr(self._SM, com["set"])
         self._SM_set = _SM_set
-        argno = len(signature(_SM_set).parameters)
-        if argno == 1:
+        #argno = len(signature(_SM_set).parameters)
+        #if argno == 1:
+        if self._type == "motor":
             # It doesn't use stack level, add void parameter
             def _aux_SM_set(self, _, value):
                 _SM_set(self, value)
